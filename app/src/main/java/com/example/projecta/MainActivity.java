84e136e9c259d80caea1d1ff07caff51573ac774
Cristhian <cristhianbay@gmail.com>
2020-05-06 10:24:13 +0200

    Se le ha dado un funcionamiento básico al login y la ventana principal

--- app/src/main/java/com/example/projecta/MainActivity.java
+++ app/src/main/java/com/example/projecta/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.projecta;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNativationView);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new HomeFragment()).commit();
+        }
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment = null;
+
+                switch (menuItem.getItemId()) {
+                    case R.id.menu_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.menu_profile:
+                        fragment = new ProfileFragment();
+                        break;
+                    case R.id.menu_settings:
+                        fragment = new SettingsFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragment).commit();
+                return false;
+            }
+        });
+
     }
 }
