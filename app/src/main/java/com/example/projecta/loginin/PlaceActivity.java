5fa90e534231422d97bc4c266447622fd49397cf
Cristhian <cristhianbay@gmail.com>
2020-05-25 21:10:07 +0200

    Actividad que mostrará la información del la plaza

---
+++ app/src/main/java/com/example/projecta/loginin/PlaceActivity.java
@@ -0,0 +1,16 @@
+package com.example.projecta.loginin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projecta.R;
+
+public class PlaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place);
+    }
+}
