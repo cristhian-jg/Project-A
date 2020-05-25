e65b601a445292d5d82652999cec69af4a46f89e
Cristhian <cristhianbay@gmail.com>
2020-05-25 21:06:32 +0200

    Primeros pasos en Google Maps y marcadores, se ha añadido un dialogo

---
+++ app/src/main/java/com/example/projecta/loginin/fragments/NavegatorFragment.java
@@ -0,0 +1,122 @@
+package com.example.projecta.loginin.fragments;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.projecta.R;
+import com.example.projecta.loginin.AddActivity;
+import com.example.projecta.loginin.MainActivity;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NavegatorFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener{
+
+    private GoogleMap mGoogleMap;
+    private MapView mMapView;
+    private View mView;
+
+    private Marker mMarker;
+
+    private ImageView btnAddUbication;
+
+    public NavegatorFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mView = inflater.inflate(R.layout.fragment_navegator, container, false);
+
+        btnAddUbication = mView.findViewById(R.id.btnAddUbication);
+
+        btnAddUbication.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
+
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mMapView = (MapView) mView.findViewById(R.id.mapView);
+
+        if (mMapView != null) {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        MapsInitializer.initialize(getContext());
+
+        mGoogleMap = googleMap;
+        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        LatLng latLng = new LatLng(38.8309575, 0.1027191);
+
+        mMarker = mGoogleMap.addMarker(new MarkerOptions()
+                        .position(latLng)
+                        .title("Testing"));
+
+        mGoogleMap.setOnMarkerClickListener(this);
+
+    }
+
+    private void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Añadir nueva ubicación");
+        builder.setMessage("Se añadirá una nueva plaza fotografica en tu ubicación actual, estás seguro?")
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        })
+                .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(getContext(), AddActivity.class);
+                startActivity(intent);
+            }
+        }).create();
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        if(marker.equals(mMarker)) {
+            
+        }
+        return false;
+    }
+}
